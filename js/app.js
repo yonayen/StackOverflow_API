6e5b67835691bf39038297eaf211cb33f02e4f08
Yonathan Ayenew <yonayen@gmail.com>
2016-06-03 05:49:15 -0700

    second function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// EVENTS WHEN INPUTS ARE FILLED OUT
 $(document).ready(function() {
 	$('.unanswered-getter').submit(function(e){
 		// prevents page from being reloaded when submitting entry in form
@@ -8,6 +9,14 @@ $(document).ready(function() {
 		var tags = $(this).find("input[name='tags']").val();
 		// calls getUnanswered function
 		getUnanswered(tags);
-
+	});
+	/* -- The answers for tags -- */
+	$('.inspiration-getter').submit(function(e){
+		e.preventDefault();
+		// empty results div
+		$('.results').html('');
+		// assign the value typed in by user to variable
+		var tag = $(this).find("input[name='answerers']").val();
+		getTopanswers(tag);
 	});
 });
